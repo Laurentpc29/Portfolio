9655d05a2c01fd00bb81ee8948c1c13b817bc9de
Laurentpc29 <laurentpc29@gmail.com>
2023-12-31 13:40:41 +0100

    test1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,7 +20,7 @@ export const Footer = () => {
               <a href="#"><img src={navIcon2} alt="Icon" /></a>
               <a href="#"><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Made by PHAN CONG Laurent with webcoded tutorial.</p>
+            <p>Made by PHAN CONG Lauren with webcoded tutorial.</p>
           </Col>
         </Row>
       </Container>
